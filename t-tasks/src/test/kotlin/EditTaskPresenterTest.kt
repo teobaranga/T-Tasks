6a42a16eb7f367791e975a4feeb25a683e4f6810
Teo Baranga <teodor08@live.com>
2018-04-17 23:32:20 -0400

    Add initial unit test (for EditTaskPresenter) - to be continued
    Unit tests are done using MockK

---
+++ t-tasks/src/test/kotlin/EditTaskPresenterTest.kt
@@ -0,0 +1,69 @@
+import com.teo.ttasks.data.local.WidgetHelper
+import com.teo.ttasks.data.model.Task
+import com.teo.ttasks.data.remote.TasksHelper
+import com.teo.ttasks.ui.activities.edit_task.EditTaskPresenter
+import com.teo.ttasks.ui.activities.edit_task.EditTaskView
+import com.teo.ttasks.util.NotificationHelper
+import io.mockk.*
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.reactivex.Single
+import io.reactivex.schedulers.TestScheduler
+import io.realm.Realm
+import org.junit.Before
+import org.junit.Test
+import java.util.*
+
+class EditTaskPresenterTest {
+
+    private val validTaskId = "123"
+
+    private val validTask = Task().apply {
+        due = Date()
+    }
+
+    @MockK
+    private lateinit var realm: Realm
+    @MockK
+    private lateinit var tasksHelper: TasksHelper
+    @MockK
+    private lateinit var widgetHelper: WidgetHelper
+    @MockK
+    private lateinit var notificationHelper: NotificationHelper
+    @RelaxedMockK
+    private lateinit var editTaskView: EditTaskView
+
+    private lateinit var editTaskPresenter: EditTaskPresenter
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        editTaskPresenter = EditTaskPresenter(tasksHelper, widgetHelper, notificationHelper)
+    }
+
+    @Test
+    fun `Load Task Info - Valid Task - Success`() {
+        staticMockk<Realm>().use {
+            // arrange
+            val testScheduler = TestScheduler()
+
+            every { Realm.getDefaultInstance() } returns realm
+
+            every {
+                tasksHelper.getTaskAsSingle(
+                        taskId = validTaskId,
+                        realm = any())
+            } returns Single.just(validTask).subscribeOn(testScheduler)
+
+            // act
+            editTaskPresenter.bindView(editTaskView)
+            editTaskPresenter.loadTaskInfo(validTaskId)
+            testScheduler.triggerActions()
+
+            // assert
+            verify(exactly = 1) { tasksHelper.getTaskAsSingle(validTaskId, any()) }
+            verify(exactly = 1) { editTaskView.onTaskLoaded(task = validTask) }
+            verify(exactly = 0) { editTaskView.onTaskLoadError() }
+        }
+    }
+}
