96a88c96f14a52efa53273f97d70c5b88bbd11c2
Teo Baranga <teodor08@live.com>
2019-11-13 20:43:23 -0500

    Add new NightMode: system
    - replace usage of deprecated MODE_NIGHT_AUTO with MODE_NIGHT_AUTO_BATTERY

--- t-tasks/src/main/java/com/teo/ttasks/util/NightHelper.kt
+++ t-tasks/src/main/java/com/teo/ttasks/util/NightHelper.kt
@@ -9,6 +9,7 @@ object NightHelper {
     const val NIGHT_AUTO = "auto"
     const val NIGHT_NEVER = "never"
     const val NIGHT_ALWAYS = "always"
+    const val NIGHT_SYSTEM = "system"
 
     fun isNight(context: Context): Boolean {
         return context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
@@ -17,8 +18,9 @@ object NightHelper {
     fun applyNightMode(nightMode: String) {
         when (nightMode) {
             NIGHT_NEVER -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            NIGHT_AUTO -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO)
+            NIGHT_AUTO -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
             NIGHT_ALWAYS -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            NIGHT_SYSTEM -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         }
     }
 }
